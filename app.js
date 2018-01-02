3eeb18f0acd16a72a9a043e715be192229074d34
talebb <taleb.benouaer@hmcts.net>
2018-01-02 20:33:51 +0000

    add static folder to express static

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/public', express.static(__dirname + '/public'));
+app.use('/static', express.static(__dirname + '/views/static'));
 app.use('/', index);
 app.use('/users', users);
 
